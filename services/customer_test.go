713a0328622931ffd658b64045d37823207d48ea
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2021-07-28 12:17:03 +0700

    add test

---
+++ services/customer_test.go
@@ -0,0 +1,104 @@
+package services
+
+import (
+	"errors"
+	"testing"
+
+	mocks "github.com/ad3n/resto/mocks/repositories"
+	"github.com/ad3n/resto/models"
+	"github.com/ad3n/resto/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Customer_Not_Found(t *testing.T) {
+	repository := mocks.CustomerRepository{}
+	repository.On("Find", 1).Return(models.Customer{}, errors.New("customer not found")).Once()
+
+	service := Customer{Repository: &repository}
+	_, err := service.Get(1)
+
+	repository.AssertExpectations(t)
+
+	assert.Equal(t, err.Error(), "customer not found")
+}
+
+func Test_Customer_Found(t *testing.T) {
+	customer := models.Customer{
+		ID:          1,
+		Name:        "Surya",
+		PhoneNumber: "087800093915",
+	}
+
+	repository := mocks.CustomerRepository{}
+	repository.On("Find", customer.ID).Return(customer, nil).Once()
+
+	service := Customer{Repository: &repository}
+	result, err := service.Get(customer.ID)
+
+	repository.AssertExpectations(t)
+
+	assert.Nil(t, err)
+	assert.Equal(t, customer.Name, result.Name)
+}
+
+func Test_Customer_Reservation(t *testing.T) {
+	customer := models.Customer{
+		ID:          1,
+		Name:        "Surya",
+		PhoneNumber: "087800093915",
+	}
+
+	order := models.Order{
+		ID:          0,
+		CustomerID:  customer.ID,
+		TableNumber: 1,
+		Status:      types.ORDER_PENDING,
+	}
+
+	repository := mocks.OrderRepository{}
+	repository.On("Saves", &order).Return(nil).Once()
+
+	service := Customer{Order: &repository}
+	result, err := service.Reservation(customer, order.TableNumber)
+
+	repository.AssertExpectations(t)
+
+	assert.Nil(t, err)
+	assert.Equal(t, order.Status, result.Status)
+}
+
+func Test_Customer_Save_Phone_Number_Not_Exist(t *testing.T) {
+	customer := models.Customer{
+		Name:        "Surya",
+		PhoneNumber: "087800093915",
+	}
+
+	repository := mocks.CustomerRepository{}
+	repository.On("Saves", &customer).Return(nil).Once()
+	repository.On("FindByPhoneNumber", customer.PhoneNumber).Return(models.Customer{}, nil).Once()
+
+	service := Customer{Repository: &repository}
+	err := service.Save(customer)
+
+	repository.AssertExpectations(t)
+
+	assert.Nil(t, err)
+}
+
+func Test_Customer_Save_Phone_Number_Exist(t *testing.T) {
+	customer := models.Customer{
+		Name:        "Surya",
+		PhoneNumber: "087800093915",
+	}
+
+	repository := mocks.CustomerRepository{}
+	repository.On("Saves", &customer).Return(nil).Once()
+	repository.On("FindByPhoneNumber", customer.PhoneNumber).Return(customer, nil).Once()
+
+	service := Customer{Repository: &repository}
+	err := service.Save(customer)
+
+	repository.AssertExpectations(t)
+
+	assert.Nil(t, err)
+}
---
+++ services/menu_test.go
@@ -0,0 +1,74 @@
+package services
+
+import (
+	"errors"
+	"testing"
+
+	mocks "github.com/ad3n/resto/mocks/repositories"
+	"github.com/ad3n/resto/models"
+	"github.com/ad3n/resto/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Menu_Get_All(t *testing.T) {
+	repository := mocks.MenuRepository{}
+	repository.On("FindAll").Return([]models.Menu{}, nil).Once()
+
+	service := Menu{Repository: &repository}
+	result, err := service.GetAll()
+
+	repository.AssertExpectations(t)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(result))
+}
+
+func Test_Menu_Not_Found(t *testing.T) {
+	repository := mocks.MenuRepository{}
+	repository.On("Find", 1).Return(models.Menu{}, errors.New("menu not found")).Once()
+
+	service := Menu{Repository: &repository}
+	_, err := service.Get(1)
+
+	repository.AssertExpectations(t)
+
+	assert.Equal(t, err.Error(), "menu not found")
+}
+
+func Test_Menu_Found(t *testing.T) {
+	menu := models.Menu{
+		ID:    1,
+		Type:  types.MENU_MAIN_COURSE,
+		Name:  "Ayam Bakar",
+		Price: 10000.0,
+	}
+
+	repository := mocks.MenuRepository{}
+	repository.On("Find", menu.ID).Return(menu, nil).Once()
+
+	service := Menu{Repository: &repository}
+	result, err := service.Get(menu.ID)
+
+	repository.AssertExpectations(t)
+
+	assert.Nil(t, err)
+	assert.Equal(t, menu.Name, result.Name)
+}
+
+func Test_Menu_Save(t *testing.T) {
+	menu := models.Menu{
+		Type:  types.MENU_MAIN_COURSE,
+		Name:  "Ayam Bakar",
+		Price: 10000.0,
+	}
+
+	repository := mocks.MenuRepository{}
+	repository.On("Saves", &menu).Return(nil).Once()
+
+	service := Menu{Repository: &repository}
+	err := service.Save(menu)
+
+	repository.AssertExpectations(t)
+
+	assert.Nil(t, err)
+}
